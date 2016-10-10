f6487acc3e318f322a143dfd36284602ca095b5a
KilianPauwels <Kilianpauwels@gmail.com>
2016-10-10 15:22:17 +0200

    kleine repo page aangemaakt

--- routing.js
+++ routing.js
@@ -6,10 +6,19 @@ app.config(function($routeProvider){
 		.when("/home"), {
 			templateUrl : "views/home.html",
 			controller : "homeController"
+		})
+
+		.when("/repos"), {
+			templateUrl :"views/repos.html",
+			controller : "repoController"
 		});
 
 });
 
 app.controller("homeController", function($scope,$http){
 	$scope.title ="Home";
+});
+
+app.controller("repoController", function($scope,$http){
+	$scope.title ="Repo's";
 });
