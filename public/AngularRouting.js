48ae442d416d5732784da75e490464a470d2dee8
KilianPauwels <Kilianpauwels@gmail.com>
2016-10-24 19:05:39 +0200

    layout in orde, alles op de juiste plek om via node te launchen

--- public/AngularRouting.js
+++ public/AngularRouting.js
@@ -3,22 +3,67 @@ var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider){
 	$routeProvider
 
-		.when("/home", {
-			templateUrl : "views/home.html",
-			controller : "homeController"
+		.when("/login", {
+			templateUrl : "views/login.html",
+			controller : "loginController"
 		})
 
-		.when("/repos", {
-			templateUrl :"views/repos.html",
+		.when("/overzicht", {
+			templateUrl : "views/overzicht.html",
+			controller : "overzichtController"
+		})
+
+		.when("/info", {
+			templateUrl : "views/info.html",
+			controller : "infoController"
+		})
+
+		.when("/commits", {
+			templateUrl : "views/commits.html",
+			controller : "commitController"
+		})
+
+		.when("/repo", {
+			templateUrl :"views/repo.html",
 			controller : "repoController"
 		});
 
 });
 
-app.controller("homeController", function($scope,$http){
+app.controller("loginController", function($scope,$http){
 	$scope.title ="Home";
 });
 
-app.controller("repoController", function($scope,$http){
-	$scope.title ="Repo's";
+app.controller("overzichtController", function($scope,$http){
+	$scope.title ="overzicht";
 });
+
+app.controller("infoController", function($scope,$http){
+	$scope.title ="info";
+});
+
+app.controller("repoController", function($scope,$http){
+	$scope.title ="Repo";
+});
+
+app.controller("commitController", function($scope,$http){
+	$scope.title ="Commits";
+	$scope.commits = [];
+
+var init = function(){
+
+	console.log("init");
+	$http.get("https://api.github.com/repos/FlorianPieters/Automatiseringbab/commits")
+	.success(function(results){
+		$scope.commits = results[0].author.login;
+		console.log(results);
+	})
+	.error(function(error){
+		console.log(error);
+	})
+}
+
+	init();
+	
+});
+
